03aa572fc7b6cb6ee5836483ad0a1b56cc377d70
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2024-03-08 15:23:16 +0800

    added rate limiter

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 // middlewares / utilities
 const globalErrorHandler = require('./v1/middlewares/globalErrorHandler');
 
 const app = express();
+
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many request from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
